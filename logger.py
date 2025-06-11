31dfecc9ffc45a6c3cc9f9c6cdd213ac91cd5de6
MRrayterat <mrrayterat@gmail.com>
2025-06-11 22:19:59 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,12 +1,1 @@
-import sys
-from datetime import datetime
 
-def log(message):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    sys.stderr.write(f"[{timestamp}] {message}\n")
-
-def main():
-    log("Text Message")
-    
-if __name__ == '__main__':
-    main()
